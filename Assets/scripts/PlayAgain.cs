cd2a2505c91c98ce5373701ea792040e7db0e6cb
dejalei <dejalei@knights.ucf.edu>
2023-02-21 00:19:22 -0500

    Created the main menu with working buttons

---
+++ Assets/scripts/PlayAgain.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PlayAgain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+    public void LoadGame()
+
+
+    {
+        SceneManager.LoadScene("scene1");
+
+
+    }
+
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    public void QuitGame()
+    {
+    Debug.Log("Quit!");
+    Application.Quit();
+    }
+}
