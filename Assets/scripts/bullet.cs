a1db19f5f190e3ec412b89eca0ead285876cde07
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2023-02-27 00:09:47 -0500

    bullet bug fixed

--- Assets/scripts/bullet.cs
+++ Assets/scripts/bullet.cs
@@ -6,15 +6,25 @@ public class bullet : MonoBehaviour
 {
     public Rigidbody rb;
     public int loseamount = 10;
+    private playerController PC;
+    void Start()
+    {
+        GameObject PlayerObject = GameObject.FindWithTag("player");
+        //stealItemText = GameObject.Find("pickupItemText");
+        if (PlayerObject != null)
+        {
+            PC = PlayerObject.GetComponent<playerController>();
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        playerController PC = collision.collider.GetComponent<playerController>();
         if (PC != null)
         {
+
             PC.losesanity(loseamount);
             Destroy(gameObject);
-
+            //PC.setCurrentItem(this);
         }
         else if (PC == null)
         {
